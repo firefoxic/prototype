ad864d92ae07636e32a77111bd79b9f598e24b43
firefoxic <firefoxic.arts@gmail.com>
2017-12-03 05:35:38 +0300

    Add polyfill for forEach() method

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,14 @@
 const checkboxList = productList.querySelectorAll('input[type=checkbox]');
 
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = (callback, thisArg) => {
+    const thisArgum = thisArg || window;
+    for (let i = 0; i < this.length; i += 1) {
+      callback.call(thisArgum, this[i], i, this);
+    }
+  };
+}
+
 checkboxList.forEach((element) => {
   const elem = element;
   if (!Math.round(Math.random())) {
